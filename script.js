5fbba41edde13db7b192229655c3f3f112323cbc
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-05 13:01:16 +0300

    feat: add HTML structure for todo list and initial references for jS

---
+++ script.js
@@ -0,0 +1,8 @@
+//* Initial references 
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
