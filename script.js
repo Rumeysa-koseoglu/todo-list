477777f3d49ab0c18d9bf0462eda5b80a8ac2ef5
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-05 14:03:22 +0300

    feat: add functionality for adding todos

--- script.js
+++ script.js
@@ -6,3 +6,40 @@ const todoList = document.querySelector(".list-group");
 const firstCardBody = document.querySelectorAll(".card-body")[0];
 const secondCardBody = document.querySelectorAll(".card-body")[1];
 const clearButton = document.querySelector("#clearButton");
+
+
+runEvents();
+
+function runEvents() {
+    form.addEventListener("submit", addTodo);
+}
+
+function addTodo(e) {
+    const inputText = addInput.value.trim();
+    if (inputText == null || inputText == "") {
+        alert("Please enter a value");
+    } else {
+        addTodoUI(inputText);
+    }
+
+    e.preventDefault();
+}
+
+function addTodoUI(newTodo) {
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justufy-content-between";
+    li.textContent = newTodo;
+
+    const a = document.createElement("a");
+    a.href = "#"
+    a.className = "delete-item";
+
+    const i = document.createElement("i");
+    i.className = "fa fa-remove";
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value = "";
+}
